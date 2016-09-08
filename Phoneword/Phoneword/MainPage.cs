07055b95ee3620b6bd30648b81ef7a515c70dec8
David Gómez <ldavid.gomez@gmail.com>
2016-09-08 16:34:56 +0200

    added main UI elements

---
+++ Phoneword/Phoneword/MainPage.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Phoneword
+{
+    class MainPage : ContentPage
+    {
+        Entry phoneNumberText;
+        Button translateButton;
+        Button callButton;
+
+        public MainPage()
+        {
+            StackLayout panel = new StackLayout();
+
+            panel.Children.Add(new Label
+            {
+                Text = "Enter a Phoneword:"
+            });
+
+            panel.Children.Add(phoneNumberText = new Entry
+            {
+                Text = "1-855-XAMARIN"
+            });
+
+            panel.Children.Add(translateButton = new Button
+            {
+                Text = "Translate"
+            });
+
+            panel.Children.Add(callButton = new Button
+            {
+                Text = "Call",
+                IsEnabled = false   
+            });
+
+            Content = panel;
+        }
+    }
+}
