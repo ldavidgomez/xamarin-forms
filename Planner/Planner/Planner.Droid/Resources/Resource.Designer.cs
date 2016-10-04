1d39f56d261bc172065b348ac03a15203c7a100a
David Gómez <ldavid.gomez@gmail.com>
2016-10-04 09:58:46 +0200

    adding splash logo

--- Planner/Planner/Planner.Droid/Resources/Resource.Designer.cs
+++ Planner/Planner/Planner.Droid/Resources/Resource.Designer.cs
@@ -2209,11 +2209,14 @@ namespace Planner.Droid
 			// aapt resource value: 0x7f0200a2
 			public const int mr_ic_play_light = 2130837666;
 			
-			// aapt resource value: 0x7f0200a4
-			public const int notification_template_icon_bg = 2130837668;
+			// aapt resource value: 0x7f0200a5
+			public const int notification_template_icon_bg = 2130837669;
 			
 			// aapt resource value: 0x7f0200a3
-			public const int splashscreen = 2130837667;
+			public const int splash_logo = 2130837667;
+			
+			// aapt resource value: 0x7f0200a4
+			public const int splashscreen = 2130837668;
 			
 			static Drawable()
 			{
